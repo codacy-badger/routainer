6bc3ea4caa374ba186c888395b44a2d1d15dd96b
David Day <dj6082013@gmail.com>
2020-01-21 10:44:58 -0800

    fix namespace

--- container/urls.py
+++ container/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'container'
 urlpatterns = [
     path('', views.index, name='index'),
     path('create/', views.create, name='create'),
--- router/urls.py
+++ router/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'router'
 urlpatterns = [
     path('', views.index, name='index'),
     path('create/', views.create, name='create'),
