3a5b78dc44123c6153959b200949f9e5a4e74a3a
David Day <dj6082013@gmail.com>
2020-01-20 16:56:19 -0800

    render models

--- container/views.py
+++ container/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from .models import Container
+
 # Create your views here.
 def index(request):
-    return render(request, 'container.html')
+    container_list = Container.objects.all()
+    context = {'container_list': container_list}
+    return render(request, 'container.html', context)
--- router/views.py
+++ router/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from .models import Router
 # Create your views here.
 def index(request):
-    return render(request, 'router.html')
+    rule_list = Router.objects.all()
+    context = {'rule_list': rule_list}
+    return render(request, 'router.html', context)
