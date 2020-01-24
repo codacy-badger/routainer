2dfea3f3f64892af76e1c99cb86d9d49eea4c290
David Day <dj6082013@gmail.com>
2020-01-24 11:32:46 -0800

    tidy methods into models.py

--- container/models.py
+++ container/models.py
@@ -1,8 +1,62 @@
 from django.db import models
 
+from router.models import Router
+import lxc
+
 
 class Container(models.Model):
     name = models.CharField(max_length=16)
     domain = models.CharField(max_length=100)
     activity = models.BooleanField()
     exposed_port = models.CharField(max_length=200)
+
+    @classmethod
+    def create(cls, name, domain, ports):
+        # Setup the container object
+        c = lxc.Container(name)
+        # Create the container rootfs
+        c.create("download", lxc.LXC_CREATE_QUIET, {"dist": "ubuntu",
+                                                    "release": "bionic",
+                                                    "arch": "amd64"})
+        # Install OpenSSH Server
+        c.attach_wait(lxc.attach_run_command,
+                      ["apt-get", "update"])
+        c.attach_wait(lxc.attach_run_command,
+                      ["apt-get", "upgrade", "-y"])
+        c.attach_wait(lxc.attach_run_command,
+                      ["apt-get", "install", "openssh-server", "-y"])
+
+        # Create container object in database
+        container = cls(name=name, domain=domain,
+                        activity=False, exposed_port=ports)
+        # add rules to router
+        for p in ports['http']:
+            container.addRoute(p, 'http')
+        for p in ports['stream']:
+            container.addRoute(p, 'stream')
+
+        return container
+
+    def destroy(self):
+        Router.objects.filter(container_name=self.name).delete()
+        c = lxc.Container(self.name)
+        c.destroy()
+        self.delete()
+
+    def activate(self):
+        c = lxc.Container(self.name)
+        c.start()
+        self.activity = (
+            c.state == "STARTING" or c.state == "RUNNING")
+        self.save()
+
+    def deactivate(self):
+        c = lxc.Container(self.name)
+        c.stop()
+        self.activity = (
+            c.state == "STOPPING" or c.state == "STOPPED")
+        self.save()
+
+    def addRoute(self, port, ptype):
+        rule = Router.create(self.name, port, ptype)
+        rule.save()
--- container/views.py
+++ container/views.py
@@ -3,9 +3,6 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 
 from .models import Container
-from router.models import Router
-
-import lxc
 
 
 def index(request):
@@ -19,20 +16,6 @@ def create(request):
     domain = request.POST['ContainerDomain']
     port_config = request.POST['ContainerPort']
 
-    # Setup the container object
-    c = lxc.Container(name)
-    # Create the container rootfs
-    c.create("download", lxc.LXC_CREATE_QUIET, {"dist": "ubuntu",
-                                                "release": "bionic",
-                                                "arch": "amd64"})
-    # Install OpenSSH Server
-    c.attach_wait(lxc.attach_run_command,
-                  ["apt-get", "update"])
-    c.attach_wait(lxc.attach_run_command,
-                  ["apt-get", "upgrade", "-y"])
-    c.attach_wait(lxc.attach_run_command,
-                  ["apt-get", "install", "openssh-server", "-y"])
-
     # setup ports
     ports = {
         'http': [],
@@ -45,45 +28,25 @@ def create(request):
             'stream': [22],
         }
 
-    # add rules to router
-    for p in ports['http']:
-        rule = Router.create(name, p, 'http')
-        rule.save()
-    for p in ports['stream']:
-        rule = Router.create(name, p, 'stream')
-        rule.save()
-
     # create container
-    row = Container(name=name, domain=domain,
-                    activity=False, exposed_port=ports)
+    row = Container.create(name=name, domain=domain, exposed_port=ports)
     row.save()
     return HttpResponseRedirect(reverse('container:index'))
 
 
 def delete(request, container_id):
     container = get_object_or_404(Container, pk=container_id)
-    Router.objects.filter(container_name=container.name).delete()
-    c = lxc.Container(container.name)
-    c.destroy()
-    container.delete()
+    container.destroy()
     return HttpResponseRedirect(reverse('container:index'))
 
 
 def active(request, container_id):
     container = get_object_or_404(Container, pk=container_id)
-    c = lxc.Container(container.name)
-    c.start()
-    container.activity = (
-        container.state == "STARTING" or container.state == "RUNNING")
-    container.save()
+    container.activate()
     return HttpResponseRedirect(reverse('container:index'))
 
 
 def inactive(request, container_id):
     container = get_object_or_404(Container, pk=container_id)
-    c = lxc.Container(container.name)
-    c.stop()
-    container.activity = (
-        container.state == "STOPPING" or container.state == "STOPPED")
-    container.save()
+    container.deactivate()
     return HttpResponseRedirect(reverse('container:index'))
