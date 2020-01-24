e823a05f41664b12aa87c53b93abc9a41d2552fb
David Day <dj6082013@gmail.com>
2020-01-24 11:41:10 -0800

    add Container unit test

--- container/tests.py
+++ container/tests.py
@@ -1,1 +1,18 @@
 from django.test import TestCase
+
+from .models import Container
+
+class ContainerModelTest(TestCase):
+
+    def test_container_operation(self):
+        default_ports = {
+            'http': 80,
+            'stream': 22
+        }
+        c = Container.create(name="Test", domain="test.lssc.club", ports=default_ports)
+        c.activate()
+        self.assertIs(c.activity, True)
+        c.stop()
+        self.assertIs(c.activity, False)
+        c.destroy()
+        self.assertIs(Container.objects.filter(name="Test").count(), 0)
